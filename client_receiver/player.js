d6ed9b0bf543647770496cd16ecaa692a9830072
Yury Pavlotsky <pavlotsky@google.com>
2018-06-19 15:16:21 -0400

    fix

--- client_receiver/player.js
+++ client_receiver/player.js
@@ -223,6 +223,6 @@ Player.prototype.requestAd_ = function(adTag, currentTime) {
  */
 Player.prototype.seek_ = function(time) {
   this.currentContentTime_ = time;
-  this.mediaElement__.currentTime = time;
+  this.mediaElement_.currentTime = time;
   this.playerManager_.play();
 };
